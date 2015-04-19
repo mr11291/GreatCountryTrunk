203de7b51abd1789d2925eb4849a1b9f7a69af5f
guyvkn <guyvkn@gmail.com>
2015-04-19 15:31:32 +0300

    Add2

--- AgileProdDAL/DataLogicCommittee.cs
+++ AgileProdDAL/DataLogicCommittee.cs
@@ -10,14 +10,7 @@ namespace AgileProdDAL
     public static class DataLogicCommittee
     {
         private static DataRepository data;
-        static DataLogicCommittee()
-        {
-            data = DataRepository.GetDataRepository();
-        }
-        public static bool AddParty(string Name,int Id)
-        {
-            if(data.GetPeople().Any(current => current.Key==Id && !data.GetMembers().Any(current1 => current1.Key== Id) && !data.GetPartyList().Any(current2 =>current2.Key== Name))
-            {
+        static
                 Person thisPerson = data.GetPeople()[Id];
                 Member newMember = new Member(Id,thisPerson.Name,thisPerson.Age,thisPerson.UserName,thisPerson.Password,thisPerson.IsVoting,Name,1);
                 data.GetPartyList().Add(Name,0);
@@ -29,4 +22,4 @@ namespace AgileProdDAL
         }
 
     }
-}
+
