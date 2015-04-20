66cf096789e34ac63a0fb1cfef5bc4aa1c1b6b4f
mr11291 <Michael_liv@hotmail.com>
2015-04-20 10:08:49 +0300

    Documentation and cleaning
    data repository documenting and cleaning
    load data base documenting and cleaning

--- AgileProdDAL/DataRepository.cs
+++ AgileProdDAL/DataRepository.cs
@@ -9,21 +9,22 @@ namespace AgileProdDAL
 {
     internal class DataRepository
     {
-
         /*
          * Class DataReposatory uses local database to create dynamic dictionary based, usable database.
          * this class DO NOT change the local data up until the user finishes (close) using the program.
          */
-        private static Dictionary<int, Person> people;       
-        private static Dictionary<int, Member> members;
-        private static Dictionary<string, int> partyList;
-        private static Dictionary<int, Message> messages;
-        private static Dictionary<int, Bank> bankAccounts;
-        private static bool praimerise;
-        private static Dictionary<int, Head> committee;
-        private static Dictionary<int, Admin> admin;
-        private static DataRepository instance;
 
+        private static Dictionary<int, Person> people;      //<id, person instance>   
+        private static Dictionary<int, Member> members;     //<id, party member instance> 
+        private static Dictionary<int, Message> messages;   //<id, message inbox instance> 
+        private static Dictionary<int, Bank> bankAccounts;  //<id, bank account instnace> 
+        private static Dictionary<int, Head> committee;     //<id, committee member instance> 
+        private static Dictionary<int, Admin> admin;        //<id, admin instance>
+        private static Dictionary<string, int> partyList;   //<party name, amount of votes recived
+        private static bool praimerise;                     //represents an on going praimeries
+        private static DataRepository instance;             //an instance of data reposetory
+
+        //constructor
         public  DataRepository(Dictionary<int, Person> peopleD, Dictionary<int, Member> membersD,
                                Dictionary<string, int> partyListD, bool praimeriseD, Dictionary<int, Head> headD,
                                Dictionary<int, Admin> adminD, Dictionary<int, Bank> bankAccountsD,
@@ -39,88 +40,82 @@ namespace AgileProdDAL
             admin = adminD;
         }
 
-
         /*
          * Those function use local data base to create dynamic dictionary based database.
          * the access to those dictionaries is global and almost every outer function uses them.
          */
 
+        //Setters Getters
         public Dictionary<int, Person> GetPeople()
         {
             return people;
         }
-
-        //public  void setPeople(Dictionary<int,Person> peopleD)
-        //{
-        //    people = peopleD;
-        //}
-
-
         public Dictionary<int, Member> GetMembers()
         {
             return members;
         }
-
-        //public void setMember(Dictionary<int, Member> memberD)
-        //{
-        //    members = memberD;
-        //}
-
         public Dictionary<int, Head> GetCommittee()
         {
             return committee;
         }
-
-        //public void setHead(Dictionary<int, Head> headD)
-        //{
-        //    head = headD;
-        //}
-
-        public Dictionary<string, int> GetPartyList()
-        {
-            return partyList;
-        }
-
-        //public void setPartyList(Dictionary<string, int> PartyListD)
-        //{
-        //    partyList=PartyListD;
-        //}
-
         public Dictionary<int, Admin> GetAdmin()
         {
             return admin;
         }
-
-        //public void setAdmin(Dictionary<string, string> adminD)
-        //{
-        //    admin = adminD;
-        //}
-
-        public bool GetPraimerise()
-        {
-            return praimerise;
-        }
-
-        //public void setPraimeries(bool praim)
-        //{
-        //    praimerise = praim;
-        //}
-
         public Dictionary<int, Message> GetMessages()
         {
             return messages;
         }
-
         public Dictionary<int, Bank> GetBankAccounts()
         {
             return bankAccounts;
         }
-
-        public static DataRepository GetDataRepository() //bad singleton
+        public Dictionary<string, int> GetPartyList()
+        {
+            return partyList;
+        }
+        public bool GetPraimerise()
+        {
+            return praimerise;
+        }
+        public static DataRepository GetDataRepository()
         {
             if (instance == null)
                 instance = LoadDataBase.LoadToDataRepository();
             return instance;
+        }   //Singleton instance
+
+        /*
+        public void setPeople(Dictionary<int, Person> peopleD)
+        {
+            people = peopleD;
         }
+
+        public void setMember(Dictionary<int, Member> memberD)
+        {
+            members = memberD;
+        }
+
+        public void setHead(Dictionary<int, Head> headD)
+        {
+            head = headD;
+        }
+
+        public void setPartyList(Dictionary<string, int> PartyListD)
+        {
+            partyList = PartyListD;
+        }
+
+        public void setAdmin(Dictionary<string, string> adminD)
+        {
+            admin = adminD;
+        }
+
+        public void setPraimeries(bool praim)
+        {
+            praimerise = praim;
+        }
+        */
+        
     }
 }
