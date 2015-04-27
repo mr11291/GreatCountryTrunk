00ad1591168d0fdaa8c0c7bfe8b9edbc1a59d2f0
mr11291 <Michael_liv@hotmail.com>
2015-04-27 18:56:09 +0300

    additions to design

--- AgileProd/LoginForm.Designer.cs
+++ AgileProd/LoginForm.Designer.cs
@@ -35,12 +35,14 @@
             this.btnEnter = new System.Windows.Forms.Button();
             this.lblCreateVol = new System.Windows.Forms.LinkLabel();
             this.inputWarning = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // lblUsername
             // 
             this.lblUsername.AutoSize = true;
-            this.lblUsername.Location = new System.Drawing.Point(62, 68);
+            this.lblUsername.Location = new System.Drawing.Point(51, 18);
             this.lblUsername.Name = "lblUsername";
             this.lblUsername.Size = new System.Drawing.Size(63, 13);
             this.lblUsername.TabIndex = 0;
@@ -49,7 +51,7 @@
             // lblPass
             // 
             this.lblPass.AutoSize = true;
-            this.lblPass.Location = new System.Drawing.Point(62, 94);
+            this.lblPass.Location = new System.Drawing.Point(51, 44);
             this.lblPass.Name = "lblPass";
             this.lblPass.Size = new System.Drawing.Size(56, 13);
             this.lblPass.TabIndex = 1;
@@ -57,14 +59,14 @@
             // 
             // username
             // 
-            this.username.Location = new System.Drawing.Point(131, 65);
+            this.username.Location = new System.Drawing.Point(120, 15);
             this.username.Name = "username";
             this.username.Size = new System.Drawing.Size(100, 20);
             this.username.TabIndex = 2;
             // 
             // password
             // 
-            this.password.Location = new System.Drawing.Point(131, 91);
+            this.password.Location = new System.Drawing.Point(120, 41);
             this.password.Name = "password";
             this.password.PasswordChar = '*';
             this.password.Size = new System.Drawing.Size(100, 20);
@@ -74,7 +76,7 @@
             // 
             this.btnEnter.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.btnEnter.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.btnEnter.Location = new System.Drawing.Point(108, 186);
+            this.btnEnter.Location = new System.Drawing.Point(99, 101);
             this.btnEnter.Name = "btnEnter";
             this.btnEnter.Size = new System.Drawing.Size(75, 23);
             this.btnEnter.TabIndex = 4;
@@ -85,7 +87,7 @@
             // lblCreateVol
             // 
             this.lblCreateVol.AutoSize = true;
-            this.lblCreateVol.Location = new System.Drawing.Point(113, 215);
+            this.lblCreateVol.Location = new System.Drawing.Point(104, 130);
             this.lblCreateVol.Name = "lblCreateVol";
             this.lblCreateVol.Size = new System.Drawing.Size(63, 13);
             this.lblCreateVol.TabIndex = 5;
@@ -97,19 +99,30 @@
             // 
             this.inputWarning.AutoSize = true;
             this.inputWarning.ForeColor = System.Drawing.Color.Red;
-            this.inputWarning.Location = new System.Drawing.Point(62, 126);
+            this.inputWarning.Location = new System.Drawing.Point(50, 74);
             this.inputWarning.Name = "inputWarning";
             this.inputWarning.Size = new System.Drawing.Size(170, 13);
             this.inputWarning.TabIndex = 6;
             this.inputWarning.Text = "username or password is incorrect!";
             this.inputWarning.Visible = false;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::AgileProd.Properties.Resources.handshakemoney;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 151);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(285, 184);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
             // LoginForm
             // 
             this.AcceptButton = this.btnEnter;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 264);
+            this.ClientSize = new System.Drawing.Size(284, 336);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.inputWarning);
             this.Controls.Add(this.lblCreateVol);
             this.Controls.Add(this.btnEnter);
@@ -120,6 +133,7 @@
             this.Name = "LoginForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "LoginForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -134,6 +148,7 @@
         private System.Windows.Forms.Button btnEnter;
         private System.Windows.Forms.LinkLabel lblCreateVol;
         private System.Windows.Forms.Label inputWarning;
+        private System.Windows.Forms.PictureBox pictureBox1;
 
 
 
