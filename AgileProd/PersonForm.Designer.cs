577873fef96db257e13c56e8739795b226c1b671
mr11291 <Michael_liv@hotmail.com>
2015-05-03 18:05:15 +0300

    changes

--- AgileProd/PersonForm.Designer.cs
+++ AgileProd/PersonForm.Designer.cs
@@ -29,10 +29,6 @@
         private void InitializeComponent()
         {
             this.voteButton = new System.Windows.Forms.Button();
-            this.partyList = new System.Windows.Forms.ComboBox();
-            this.rvkbtn = new System.Windows.Forms.Button();
-            this.addtoprty = new System.Windows.Forms.Button();
-            this.AddToParty = new System.Windows.Forms.ComboBox();
             this.CommitteListLabel = new System.Windows.Forms.Label();
             this.MemberListLabel = new System.Windows.Forms.Label();
             this.ListOf2 = new System.Windows.Forms.ListView();
@@ -47,10 +43,6 @@
             this.blankTab.Controls.Add(this.ListOf);
             this.blankTab.Controls.Add(this.MemberListLabel);
             this.blankTab.Controls.Add(this.CommitteListLabel);
-            this.blankTab.Controls.Add(this.AddToParty);
-            this.blankTab.Controls.Add(this.addtoprty);
-            this.blankTab.Controls.Add(this.rvkbtn);
-            this.blankTab.Controls.Add(this.partyList);
             this.blankTab.Controls.Add(this.voteButton);
             this.blankTab.Text = "Person";
             // 
@@ -63,43 +55,6 @@
             this.voteButton.Text = "Vote";
             this.voteButton.UseVisualStyleBackColor = true;
             this.voteButton.Click += new System.EventHandler(this.voteButton_Click);
-            // 
-            // partyList
-            // 
-            this.partyList.FormattingEnabled = true;
-            this.partyList.Location = new System.Drawing.Point(445, 95);
-            this.partyList.Name = "partyList";
-            this.partyList.Size = new System.Drawing.Size(169, 21);
-            this.partyList.TabIndex = 13;
-            this.partyList.Text = "select...";
-            // 
-            // rvkbtn
-            // 
-            this.rvkbtn.Location = new System.Drawing.Point(6, 35);
-            this.rvkbtn.Name = "rvkbtn";
-            this.rvkbtn.Size = new System.Drawing.Size(92, 23);
-            this.rvkbtn.TabIndex = 15;
-            this.rvkbtn.Text = "Revoke Voting";
-            this.rvkbtn.UseVisualStyleBackColor = true;
-            // 
-            // addtoprty
-            // 
-            this.addtoprty.Location = new System.Drawing.Point(6, 64);
-            this.addtoprty.Name = "addtoprty";
-            this.addtoprty.Size = new System.Drawing.Size(92, 23);
-            this.addtoprty.TabIndex = 16;
-            this.addtoprty.Text = "Add To Party";
-            this.addtoprty.UseVisualStyleBackColor = true;
-            // 
-            // AddToParty
-            // 
-            this.AddToParty.FormattingEnabled = true;
-            this.AddToParty.Location = new System.Drawing.Point(484, 161);
-            this.AddToParty.Name = "AddToParty";
-            this.AddToParty.Size = new System.Drawing.Size(130, 21);
-            this.AddToParty.TabIndex = 17;
-            this.AddToParty.Text = "select...";
-            this.AddToParty.Visible = false;
             // 
             // CommitteListLabel
             // 
@@ -156,10 +111,6 @@
         #endregion
 
         private System.Windows.Forms.Button voteButton;
-        private System.Windows.Forms.ComboBox partyList;
-        private System.Windows.Forms.Button rvkbtn;
-        private System.Windows.Forms.Button addtoprty;
-        private System.Windows.Forms.ComboBox AddToParty;
         private System.Windows.Forms.Label CommitteListLabel;
         private System.Windows.Forms.Label MemberListLabel;
         private System.Windows.Forms.ListView ListOf2;
